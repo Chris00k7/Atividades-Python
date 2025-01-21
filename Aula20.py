76f823b9f8c50dfcee35e5b22b3b7394e96bd9dd
Chris00k7 <Jeanpereira00k7@gmail.com>
2025-01-21 00:46:09 -0300

    first commit

---
+++ Aula20.py
@@ -0,0 +1,9 @@
+Primeiro_valor = input ('Digite um valor ')
+Segundo_valor = input ('Digite mais um valor ')
+
+if Primeiro_valor > Segundo_valor:
+    print('O primeiro valor é maior que o segundo valor')
+elif Primeiro_valor == Segundo_valor:
+    print('Os dois valores são iguais')
+else:
+    print('O segundo valor é maior que o primeiro valor')
